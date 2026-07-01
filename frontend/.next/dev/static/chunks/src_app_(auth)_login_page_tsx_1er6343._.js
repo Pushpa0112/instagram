f0b27a17693633a23515196a0f9392171b711513
Pushpa <Pushpa.k@edukyu.com>
2026-07-01 11:38:31 +0530

    chore: remove .next from tracking and update gitignore

--- frontend/.next/dev/static/chunks/src_app_(auth)_login_page_tsx_1er6343._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_react-hook-form_dist_index_esm_mjs_0b0kr13._.js","static/chunks/node_modules_zod_v4_1ujpcun._.js","static/chunks/node_modules_axios_lib_1dybt4p._.js","static/chunks/node_modules_@base-ui_react_1t4gb5g._.js","static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_0q79z23._.js","static/chunks/node_modules_0qejdqa._.js","static/chunks/src_1atnn81._.js"],
-    source: "dynamic"
-});
